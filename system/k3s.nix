a7600955ab4c6165990956669078074d5a019897
Lorenzo Zolfanelli <lorenzo@zolfanelli.it>
2024-04-07 02:07:18 +0200

    k3s: add tls san with domani suffix

--- system/k3s.nix
+++ system/k3s.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
@@ -14,6 +14,7 @@
   services.k3s.extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally addp additional args to k3s
     "--kubelet-arg=feature-gates=NodeSwap=true" # Enable use of swap memory
+    "--tls-san=${config.networking.hostName}.zolfa.nl"
   ];
   environment.systemPackages = [
     pkgs.k3s
