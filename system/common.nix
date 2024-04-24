6924d596aea0dc1f1fcc7b87fa27b8b0a74ff42c
Lorenzo Zolfanelli <lorenzo@zolfanelli.it>
2024-04-24 17:42:59 +0200

    fix byobu autostart

--- system/common.nix
+++ system/common.nix
@@ -64,7 +64,7 @@
   ];
   environment.etc."zprofile.local".text = ''
     # Autostart byobu multiplexer for all users
-    ${pkgs.byobu}/bin/byobu-launcher
+    . ${pkgs.byobu}/bin/byobu-launcher
   '';
   
   system.stateVersion = "24.05";
