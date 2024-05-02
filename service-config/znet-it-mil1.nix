1218a771f62ad9b341cc031da41d2375f19d9f16
Lorenzo Zolfanelli <lorenzo@zolfanelli.it>
2024-05-02 17:30:09 +0200

    k3s: test exposing metrics on znet-it-mil1

--- service-config/znet-it-mil1.nix
+++ service-config/znet-it-mil1.nix
@@ -13,6 +13,10 @@
     extraFlags = toString [
       "--tls-san=${config.networking.hostName}.zolfa.nl"
       "--tls-san=eu-south-1.zolfa.nl"
+      "--kube-controller-manager-arg=bind-address=0.0.0.0"
+      "--kube-proxy-arg=metrics-bind-address=0.0.0.0"
+      "--kube-scheduler-arg=bind-address=0.0.0.0"
+      "--etcd-expose-metrics"
     ];
   };
 }
