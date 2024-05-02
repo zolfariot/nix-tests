7371da7ea62705a92b397d6b6ee6c82eccac5ca1
Lorenzo Zolfanelli <lorenzo@zolfanelli.it>
2024-05-02 20:57:41 +0200

    znet-it-mil1: open extra ports for metrics

--- service-config/znet-it-mil1.nix
+++ service-config/znet-it-mil1.nix
@@ -7,12 +7,21 @@
     sopsFile = ../secrets/k3s-eu-south-1.yaml;
   };
 
+  networking.firewall.allowedTCPPorts = [
+    10257 # k3s, used for metrics (controller-manager)
+    2381  # k3s, used for metrics (etcd)
+    10249 # k3s, used for metrics (kube-proxy)
+    10259 # k3s, used for metrics (kube-scheduler)
+    10250 # k3s, used for metrics
+  ];
+
   services.k3s = {
     clusterInit = true;
     tokenFile = "/run/secrets/k3s-token";
     extraFlags = toString [
       "--tls-san=${config.networking.hostName}.zolfa.nl"
       "--tls-san=eu-south-1.zolfa.nl"
+      # k3s metrics
       "--kube-controller-manager-arg=bind-address=0.0.0.0"
       "--kube-proxy-arg=metrics-bind-address=0.0.0.0"
       "--kube-scheduler-arg=bind-address=0.0.0.0"
